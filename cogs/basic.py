a6e25ad43d3e586afc25c3c6f1d56010c1ff2113
Jelly <64805926+jellytoaster@users.noreply.github.com>
2024-01-22 17:49:37 -0500

    add /role

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,11 +1,16 @@
 import disnake
 import config
+import classes.enums
+import classes.role
+import string
 from disnake.ext import commands
 
 class basic(commands.Cog):
     changelogs = [("1.0.0", disnake.Embed(title="**Anarchic 1.0.0 - Initial Release**", colour=disnake.Colour(0xeb8110), description="Welcome to Anarchic, a totally safe town where absoloutely nothing bad can happen! *(citations needed)*").add_field(name="**New Features**", value="<:add:1034558781847261194> Meet the first batch of roles! Interrogate with the <:copicon2:889672912905322516> **Cop**, Kill with the <:maficon:891739940055052328> **Mafioso**, and do much more with the <:vgicon:890339050865696798> **Vigilante**, <:docicon2:890333203959787580> **Doctor**, and <:consicon2:890336628269281350> **Consort**.\n<:add:1034558781847261194> Want to play Anarchic? Quickly join and leave a game with `/join` and `/leave`.\n<:add:1034558781847261194> Maybe you don't know what a role does. `/roles` can help! It shows you every role in Anarchic and includes a handy dandy dropdown for you to select a role to see in detail.", inline=False), "Initial release!", "<:anarchic:1009910329322512464>"),
                   ("1.1.0", disnake.Embed(title="**Anarchic 1.1.0 - Neutrals Update**", colour=disnake.Colour(0x8c8c8c), description="Two new roles arrive, but they don't seem to be siding with anyone... I wonder what they want?").add_field(name="**New Features**", value="<:add:1034558781847261194> Beware of the <:jesticon2:889968373612560394> **Jester**! This lunatic may seem harmless, but they can haunt and roleblock you if you <:lynch:1010226047456915547> **lynch** them.\n<:add:1034558781847261194> Somebody has wronged you... and you must get revenge! This is the <:hhicon2:891429754643808276> **Headhunter**, who must lynch two of their targets before they can rest in peace.\n<:add:1034558781847261194> Need help? `/help` shows you every command in the bot if you really need it.\n<:add:1034558781847261194> Look in the past! Use `/changelog` to see what new features have been added update after update.\n<:add:1034558781847261194> Share setups with your friends! Use `/setup export` to get a copy-pastable version of your current setup. Your friends can import your setup with `/setup import`!", inline=False).add_field(name="**Game Changes**", value="<:change:1034559083627429918> Fate-deciding results now say none if nobody votes\n<:change:1034559083627429918> Empty setups now say \"Empty\" for setup names", inline=False).add_field(name="**Bug Fixes**", value="<:remove:1198401946567049327> Fixed issues with party commands with default avatars\n<:remove:1198401946567049327> /host now works properly\n<:remove:1198401946567049327> Fixed issues with /setup while there is no party", inline=False), "Neutrals and more", "<:hhicon2:891429754643808276>"),
-                  ("1.1.1", disnake.Embed(title="**Anarchic 1.1.1**", colour=disnake.Colour(0x8c8c8c), description="Boring minor update").add_field(name="**Game Changes**", value="<:change:1034559083627429918> Importing setups now have a confirmation"), "Minor Update", "🔧")]
+                  ("1.1.1", disnake.Embed(title="**Anarchic 1.1.1**", colour=disnake.Colour(0x8c8c8c), description="Boring minor update").add_field(name="**Game Changes**", value="<:change:1034559083627429918> Importing setups now have a confirmation"), "Minor Update", "🔧"),
+                  ("1.2.0", disnake.Embed(title="**Anarchic 1.2.0 - Investigatives Update**", colour=disnake.Colour(0x318004), description="Two new roles arrive, but they don't seem to be siding with anyone... I wonder what they want?").add_field(name="**New Features**", value="<:add:1034558781847261194> Beware of the <:jesticon2:889968373612560394> **Jester**! This lunatic may seem harmless, but they can haunt and roleblock you if you <:lynch:1010226047456915547> **lynch** them.\n<:add:1034558781847261194> Somebody has wronged you... and you must get revenge! This is the <:hhicon2:891429754643808276> **Headhunter**, who must lynch two of their targets before they can rest in peace.\n<:add:1034558781847261194> Need help? `/help` shows you every command in the bot if you really need it.\n<:add:1034558781847261194> Look in the past! Use `/changelog` to see what new features have been added update after update.\n<:add:1034558781847261194> Share setups with your friends! Use `/setup export` to get a copy-pastable version of your current setup. Your friends can import your setup with `/setup import`!", inline=False).add_field(name="**Game Changes**", value="<:change:1034559083627429918> Fate-deciding results now say none if nobody votes\n<:change:1034559083627429918> Empty setups now say \"Empty\" for setup names", inline=False).add_field(name="**Bug Fixes**", value="<:remove:1198401946567049327> Fixed issues with party commands with default avatars\n<:remove:1198401946567049327> /host now works properly\n<:remove:1198401946567049327> Fixed issues with /setup while there is no party", inline=False), "Neutrals and more", "<:hhicon2:891429754643808276>")
+                  ]
 
     def getChangelog(version:str):
         return next((i for i in basic.changelogs if i[0] == version), None)
@@ -13,6 +18,14 @@ class basic(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.slash_command(name="role", description="Get info on a specific role", options=[disnake.Option(name="name", description="What role?", type=disnake.OptionType.string, required=True)])
+    async def role(self, inter:disnake.ApplicationCommandInteraction, name):
+        if (classes.role.Role.toRole(name) == None):
+            await inter.response.send_message("That's not a role!", ephemeral=True)
+            return
+
+        await inter.response.send_message(embed=classes.role.Role.toRole(name).roleEmbed)
 
     @commands.slash_command(name="roles", description="Get a description of every role in Anarchic")
     async def roles(self, inter: disnake.ApplicationCommandInteraction):
@@ -119,3 +132,7 @@ class basic(commands.Cog):
         embed.add_field(name="Anarchic Dev Team", value=f"**:art: Artists - evanzhaaa, temporary0533**\n**:computer: Programmers - jellytoaster**\n**:video_game: Designer - c.atasyl**")
 
         await inter.response.send_message(embed=embed)
+
+    @role.autocomplete("name")
+    async def autoCompleteRole(inter, input):
+        return [string.capwords(e.name) for e in classes.role.Role.allRoles if input.lower() in e.name.lower()]
