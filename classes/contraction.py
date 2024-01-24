df31286bc55a353c9ff19eebbdc0450fd87f28f6
Jelly <64805926+jellytoaster@users.noreply.github.com>
2024-01-23 20:18:29 -0500

    add emojis to contraction

--- classes/contraction.py
+++ classes/contraction.py
@@ -1,25 +1,30 @@
+import string
+
 class Contraction():
     allContractions = []
     def __init__(self, faction, type, displayname, emoji = "") -> None:
         self.faction = faction
         self.type = type
         self.display_name = displayname
+        self.emoji = emoji
 
         Contraction.allContractions.append(self)
 
+    def getContraction(name):
+        return [e for e in Contraction.allContractions if e.display_name == string.capwords(name.lower())][0]
 
     def initContractions():
         # Town Roles
-        Contraction("town", "", "Random Town")
-        Contraction("town", "investigative", "Town Investigative")
-        Contraction("town", "killing", "Town Killing")
-        Contraction("town", "protective", "Town Protective")        
+        Contraction("town", "", "Random Town", "🏠")
+        Contraction("town", "investigative", "Town Investigative", "🔍")
+        Contraction("town", "killing", "Town Killing", "🔫")
+        Contraction("town", "protective", "Town Protective", "💉")        
 
         # Mafia Roles
-        Contraction("mafia", "", "Random Mafia")
-        Contraction("mafia", "killing", "Mafia Killing")
-        Contraction("mafia", "support", "Mafia Support")     
+        Contraction("mafia", "", "Random Mafia", "🌹")
+        Contraction("mafia", "killing", "Mafia Killing", "🔪")
+        Contraction("mafia", "support", "Mafia Support", "💖")     
 
         # Neutral Roles
-        Contraction("neutral", "", "Random Neutral")
-        Contraction("neutral", "evil", "Neutral Evil")
+        Contraction("neutral", "", "Random Neutral", "🤷‍♂️")
+        Contraction("neutral", "evil", "Neutral Evil", "🪓")
