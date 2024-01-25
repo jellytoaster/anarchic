f0a5f78c727362361c366a1b830302721e430871
Jelly <64805926+jellytoaster@users.noreply.github.com>
2024-01-24 19:56:24 -0500

    reset func for resetting frames

--- classes/investigationResults.py
+++ classes/investigationResults.py
@@ -1,8 +1,19 @@
+import copy
+import classes.player
+
 class investigationResults():
     def __init__(self, cop:bool, consig:str):
+        self.realCopSuspicious = cop
+
         self.copSuspicious = cop
         self.consigliereFlavorText = consig
 
         # investigative roles will use this because framer could modify this without breaking actual targets
         self.lookoutVisitedBy = []
         self.trackerTargetted = []
+
+    def reset(self, player:classes.player.Player):
+        self.copSuspicious = self.realCopSuspicious
+
+        self.lookoutVisitedBy = player.whoVisitedMe()
+        self.trackerTargetted = copy.copy(player.nightTargettedPlayers)
